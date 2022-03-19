2e1e5160d4ba677d35ac062eb0a1d3c50179f2e7
MaxShady <max.shady1611@gmail.com>
2022-03-19 13:19:37 +0330

    Get items in cart

--- core/routes/cart.js
+++ core/routes/cart.js
@@ -6,8 +6,16 @@ const { hasToken } = require("../../middlewares/token-validator");
 const valid = require("../../middlewares/joi-validator");
 const validator = require("../../models/cart-model");
 
-router.get("/", async (req, res) => {
-
+router.get("/", hasToken, async (req, res) => {
+    await prisma.purchases.findMany({
+       where: {
+           UserEmail: req.email
+       }
+    }).then(data => {
+        return res.json(data);
+    }).catch(err => {
+        return res.status(400).json(err);
+    });
 });
 
 router.post("/add", [hasToken, valid(validator)], async (req, res) => {
