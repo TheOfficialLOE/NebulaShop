fa1c8a57dedd3f881fc0fa5edf17538dacf5ee8f
MaxShady <max.shady1611@gmail.com>
2022-03-21 15:20:45 +0330

    Add query builder to products cms

--- cms/routes/products.js
+++ cms/routes/products.js
@@ -1,62 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
-const { cms } = require("../../middlewares/token-validator");
+const {cms} = require("../../middlewares/token-validator");
 const valid = require("../../middlewares/joi-validator");
 const validator = require("../../models/products-model");
+const {create} = require("../../utilities/query-builder");
 
 // We'll manage products CRUD operations here...
 
 
 router.post("/newProduct", [cms, valid(validator.product)], async (req, res) => {
-    await prisma.products.create({
-        data: {
-            Name: req.body.Name,
-            Description: req.body.Description,
-            Info: req.body.Info,
-            BrandName: req.body.Brand,
-            Remaining: req.body.Remaining,
-            CreatedBy: req.email,
-            Price: req.body.Price
-        },
-        include: {
-            Purchases: true
-        }
-    }).then(data => {
-
-        return res.json(data);
 
 
-    }).catch(err => {
-        if (err.code === "P2003")
+    const product = await create(prisma.products, {
+        Name: req.body.Name,
+        Description: req.body.Description,
+        Info: req.body.Info,
+        BrandName: req.body.Brand,
+        Remaining: req.body.Remaining,
+        CreatedBy: req.email,
+        Price: req.body.Price
+    }, {
+        Purchases: true
+    });
+
+    if (product.success)
+        return res.json(product);
+    else {
+        if (product.data.code === "P2003")
             return res.status(400).json("Brand not found")
         return res.status(400).json("Error occurred...");
-    });
+    }
+
 });
 
 router.post("/newBrand", [cms, valid(validator.brand)], async (req, res) => {
-    await prisma.brands.create({
-        data: {
-            Name: req.body.Name,
-            BrandDetails: req.body.Details,
-            CreatedBy: req.email
-        },
-        include: {
-            Products: true
-        }
-    }).then(data => {
-
-        return res.json(data);
 
 
-    }).catch(err => {
-        if (err.code === "P2002")
-            return res.status(400).json("Brand already exists...");
-        else if (err.code === "P2003")
-            return res.status(403).json("User not found...")
-        return res.status(400).json("Error occurred...");
+    const brand = await create(prisma.brands, {
+        Name: req.body.Name,
+        BrandDetails: req.body.Details,
+        CreatedBy: req.email
+    }, {
+        Products: true
     });
+
+    if (brand.success)
+        return res.json(brand);
+    else {
+        if (brand.code === "P2002")
+            return res.status(400).json("Brand already exists...");
+        else if (brand.code === "P2003")
+            return res.status(403).json("User not found...");
+        return res.status(400).json("Error occurred...");
+    }
+
+
 });
 
 
