f7b791ca64d1e0689893e48b3b3bac9ecfd6f241
MaxShady <max.shady1611@gmail.com>
2022-03-21 18:30:30 +0330

    Add comments route

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const main = require("../core/routes/main");
 const cart = require("../core/routes/cart");
+const comments = require("../core/routes/comments");
 const authentication = require("../core/routes/auth");
 const products = require("../cms/routes/products");
 const purchases = require("../core/routes/purchases");
@@ -9,6 +10,7 @@ module.exports = (app) => {
     app.use(express.json());
     app.use("/products", main);
     app.use("/cart", cart);
+    app.use("/comments", comments);
     app.use("/auth", authentication);
     app.use("/purchase", purchases);
     app.use("/cms", products);
