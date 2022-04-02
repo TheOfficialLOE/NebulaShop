da849d217e04a5bad8c1f88e7cb8c20119f59bb2
MaxShady <max.shady1611@gmail.com>
2022-04-02 14:06:08 +0430

    Update remaining after a purchase

--- core/routes/purchases.js
+++ core/routes/purchases.js
@@ -1,25 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 const _ = require("lodash");
 const { hasToken } = require("../../middlewares/token-validator");
 const valid = require("../../middlewares/joi-validator");
 const validator = require("../../models/purchase-model");
-const { findMany, create } = require("../../utilities/query-builder");
+const { findMany, create, update } = require("../../utilities/query-builder");
 
 router.post("/buy", [hasToken, valid(validator)], async (req, res) => {
+
     const purchase = await create(prisma.purchases, {
-        ProductId: Number(req.body.ProductId),
+        ProductId: req.body.ProductId,
         UserEmail: req.email,
-        Count: Number(req.body.Count),
+        Count: req.body.Count,
         Address: req.body.Address
     });
+
     if (purchase.success) {
-        purchase.data = _.pick(purchase.data, ["ProductId", "UserEmail", "Count", "Address"])
-        return res.json(purchase);
-    }
-    else {
+
+
+        const changedRemaining = await update(prisma.products, {
+            Id: req.body.ProductId
+        }, {
+            Remaining: {
+                decrement: req.body.Count
+            }
+        });
+
+        if (changedRemaining.success){
+            purchase.data = _.pick(purchase.data, ["ProductId", "UserEmail", "Count", "Address"])
+            return res.json(purchase);
+        }
+        else {
+            return res.status(400).json("Error occurred...");
+        }
+
+
+    } else {
         if (purchase.data.code === "P2003")
             return res.status(400).json("Product not found...")
         return res.status(400).json("Error occurred...");
